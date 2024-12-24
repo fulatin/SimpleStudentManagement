7f9cb039c3bc6da2e212fc471ee7f6504178cd78
CamelliaGreen <1298140174@qq.com>
2024-12-24 13:02:11 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main() {
   char buffer[MAX_NAME_LEN] = "dwdw";
   addStudent(buffer, 19);
   printf("%d", findStudentByName(buffer)->age);
+  printf("114514");
   printf("Haven't finished yet");
 }
