3666c7970893bb2a57f3edf051340ba0c917f9e8
fulatin <shijiahui2006hh@qq.com>
2024-12-24 16:01:21 +0800

    搭建了命令行应用程序的基本框架

--- main.c
+++ main.c
@@ -3,7 +3,31 @@
 #include <stdio.h>
 int main() {
   char buffer[MAX_NAME_LEN] = "dwdw";
-  addStudent(buffer, 19);
-  printf("%d", findStudentByName(buffer)->age);
-  printf("Haven't finished yet");
+
+  int shouldExit = 0;
+
+  while (!shouldExit) {
+    printf("请输入你要进行的操作：\n"
+           "(1) 添加一个学生\n"
+           "(2) 删除一个学生\n"
+
+           // TODO
+
+           "(3) 输出所有学生\n"
+           "(4) 退出\n");
+    int op;
+    scanf("%d", &op);
+    if (op == 1) {
+
+    } else if (op == 2) {
+
+    } else if (op == 3) {
+
+    } else if (op == 4) {
+      saveToFile("data");
+      shouldExit = 1;
+    }
+  }
+
+  return 0;
 }
