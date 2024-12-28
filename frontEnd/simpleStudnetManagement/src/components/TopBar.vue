993534b64983cca4ab509ef46fcff23ec26c96b6
fulatin <shijiahui2006hh@qq.com>
2024-12-28 22:57:11 +0800

    Start frontend work

---
+++ frontEnd/simpleStudnetManagement/src/components/TopBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+function a() {
+  alert("that is it")
+}
+
+</script>
+
+
+<template>
+  <div id="topbar">
+    <h1>SimpleStudentManagenent</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  margin: 4px;
+}
+
+#topbar {
+  background-color: lightblue;
+  border-color: lightskyblue;
+  border-radius: 10px;
+  padding-left: 10px;
+  border-style: solid;
+  text-align: center;
+  box-shadow: lightblue;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  color: #1C304A;
+}
+</style>
